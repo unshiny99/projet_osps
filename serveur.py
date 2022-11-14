ae788c472052c25e73a819d2f9e100e852bcaf63
Maxime Frémeaux <maxime.fremeaux2@gmail.com>
2022-11-14 21:03:06 +0100

    ajout commentaires et gestion KeyBoardInterrupt watchdog

--- serveur.py
+++ serveur.py
@@ -3,13 +3,15 @@
 #
 # Création d'un segment mémoire partagée et invocation du serveur secondaire
 # Fonctionnalité basique du watchdog (surveillance d'état des serveurs conjoints)
+# Programme conçu pour les distributions Linux
 #
-# Version 08/11/2022
+# Version 14/11/2022
 # Réalisé par Maxime Frémeaux & Khalil Bedjaoui
 #
 import multiprocessing
 import os
 import platform
+import psutil
 import time
 from multiprocessing import shared_memory, process
 from threading import Timer
@@ -34,7 +36,7 @@ def serveurPrincipal():
     # Ouverture en lecture est bloquante si pas de rédacteur
     fifo2 = open(pathtube2, "r")
 
-    # TRAITEMENT SOUHAITE ICI
+    # écriture à la chaîne de 3 chaînes de caractère vers la mémoire partagée
     for i in range(3):
         print('Processus principal prêt pour échanger des messages...')
 
@@ -49,9 +51,11 @@ def serveurPrincipal():
         line = fifo2.readline()
         print("Message recu : " + line)
 
+    # boucle infinie pour ne pas fermer les tubes permettant la communication conjointe des serveurs
     while True:
         continue
 
+    # début code ignoré dû à la boucle au dessus
     print('Fermeture du tube1...')
     fifo1.close()
     print('Fermeture du tube2...')
@@ -66,11 +70,10 @@ def serveurPrincipal():
     shm_segment2.close()
     shm_segment1.close()
     shm_segment1.unlink()
+    # fin de code ignoré et de la fonction
 
 def serveurSecondaire():
     # Création du segment mémoire partagée + accès à son "nom" (utilisé pour générer une clef)
-    # (le nom peut être généré automatiquement, mais l'avantage de le fixer est que les n processus
-    # qui accèdent au même segment ont "juste besoin de connaitre ce nom pour y accéder")
     shm_segment1 = shared_memory.SharedMemory(name=_name, create=False, size=_size)
     print ('Nom du segment mémoire partagée :', shm_segment1.name)
 
@@ -87,13 +90,14 @@ def serveurSecondaire():
     print ('Contenu du segment mémoire partagée en octets via second accès :', bytes(shm_segment2.buf))
 
     print('Ouverture du tube1 en lecture... (serveur S)')
-    # Ouverture en lecture est bloquante si pas de rédacteur
+    # Ouverture en lecture (bloquante si pas de rédacteur)
     fifo1 = open(pathtube1, "r")
 
     print('Ouverture du tube2 en écriture... (serveur S)')
-    # Ouverture en écriture est bloquante si pas de lecteur
+    # Ouverture en écriture (bloquante si pas de lecteur)
     fifo2 = open(pathtube2, "w")
 
+    # écriture à la chaîne de 3 chaînes de caractère vers la mémoire partagée
     for i in range(3):
         print('Processus secondaire prêt pour échanger des messages...')
 
@@ -108,6 +112,7 @@ def serveurSecondaire():
         fifo2.write(f"Message du process secondaire : {i}!\n")
         fifo2.flush() # vider le buffer
 
+    # boucle infinie pour ne pas fermer les tubes permettant la communication conjointe des serveurs
     while True:
         continue
 
@@ -117,21 +122,16 @@ def watchdog():
     print("pid de SP : " + str(processes[0].pid))
     print("pid de SS : " + str(processes[1].pid))
 
-    # print(processes[0].is_alive())
-    # print(processes[0].exitcode)
-    # print(processes[1].is_alive())
-    # print(processes[1].exitcode)
-
     # si SS est kill mais SP en vie, tenter de relancer SS
     if(processes[0].is_alive() and not processes[1].is_alive()):
         print("Relancement du serveur secondaire")
         processes[1] = multiprocessing.Process(target=serveurSecondaire)
         processes[1].start()
 
-    # si SP est kill mais SS en vie, tuer SP -- et le watchdog
-    # tue le processus courant (et son fils serveur secondaire) et en relance un (ainsi que le fils)
+    # si SP est kill mais SS en vie, tuer SP
+    # tue le processus du serveur secondaire et relance un serveur principal puis un serv. secondaire
     # REMARQUE : le processus du serveur secondaire du SP éteint est en mode Zombie et non complètement détruit
-    if(not processes[0].is_alive() and processes[1].is_alive()):
+    elif(not processes[0].is_alive() and processes[1].is_alive()):
         print("Relancement du serveur principal")
         processes[0] = multiprocessing.Process(target=serveurPrincipal)
         processes[0].start()
@@ -140,11 +140,16 @@ def watchdog():
         processes[1] = multiprocessing.Process(target=serveurSecondaire)
         processes[1].start()
 
-    # attente conjointe processus
-    processes[0].join(1)
-    processes[1].join(1)
+    try:
+        # attente conjointe processus
+        processes[0].join(1)
+        processes[1].join(1)
+    except KeyboardInterrupt:
+        # si programme principal interrompu, tuer les processsus fils
+        processes[0].terminate()
+        processes[1].terminate()
 
-# programme conçu pour les distributions Linux
+# vérification du système d'exploitation
 if(platform.system() == 'Linux'):
     # Création du segment mémoire partagée + accès à son "nom" (utilisé pour générer une clef)
     try:
@@ -184,19 +189,21 @@ if(platform.system() == 'Linux'):
     os.mkfifo(pathtube2, 0o0600)
 
     # création des processus en multiprocessing (initialisation)
-    p1 = multiprocessing.Process(target=serveurPrincipal)
-    p2 = multiprocessing.Process(target=serveurSecondaire)
-    processes[0] = p1
-    processes[1] = p2
+    processes[0] = multiprocessing.Process(target=serveurPrincipal)
+    processes[1] = multiprocessing.Process(target=serveurSecondaire)
 
+    # tentative ajout option démon pour les tuer si le père est tué (ne fonctionnait pas)
+    # processes[0].daemon = True
+    # processes[1].daemon = True
+    
     # démarrage des processus serveur
     processes[0].start()
     processes[1].start()
 
-    # lancement boucle watchdog
+    # lancement de la boucle watchdog :
     # vérifie toutes les 2 secondes l'état des processus fils (SP et SS)
-    timer = RepeatTimer(1, watchdog) #  , args=("bar",) # si besoin d'argument à passer
+    timer = RepeatTimer(1, watchdog) # , args=("bar",) # si besoin d'argument à passer
     timer.start()
     time.sleep(2)
-else:
+else: # autres systèmes d'exploitation
     print(platform.system() + " n'est pas (encore) supporté :/")
